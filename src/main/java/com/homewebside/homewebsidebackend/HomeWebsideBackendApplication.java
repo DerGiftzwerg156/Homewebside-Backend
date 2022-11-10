d582fcd8e3195a7d3c095187d202579e6c95fda0
DerGiftzwerg156 <50719189+DerGiftzwerg156@users.noreply.github.com>
2022-11-10 21:08:00 +0100

    Bug Fix?

--- src/main/java/com/homewebside/homewebsidebackend/HomeWebsideBackendApplication.java
+++ src/main/java/com/homewebside/homewebsidebackend/HomeWebsideBackendApplication.java
@@ -1,13 +1,10 @@
 package com.homewebside.homewebsidebackend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class,
-		ManagementWebSecurityAutoConfiguration.class })
-
+@SpringBootApplication
 public class HomeWebsideBackendApplication {
 
 	public static void main(String[] args) {
