ed3066ba7fb731d66e3967cdb20596c9f49fc913
DerGiftzwerg156 <50719189+DerGiftzwerg156@users.noreply.github.com>
2022-11-10 14:42:11 +0100

    Ready for first Deploy

--- src/test/java/com/homewebside/homewebsidebackend/HomeWebsideBackendApplicationTests.java
+++ src/test/java/com/homewebside/homewebsidebackend/HomeWebsideBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.homewebside.homewebsidebackend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HomeWebsideBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.homewebside.homewebsidebackend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HomeWebsideBackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
